da07a8a914f87daa7f24849bb0cc6529362216c0
Sufini Mohamed <108342077+Sufinity@users.noreply.github.com>
2022-08-06 10:01:02 +0800

    Update snoopy.py

--- snoopy.py
+++ snoopy.py
@@ -1,4 +1,25 @@
 import streamlit as st
-
 import numpy as np
 import pandas as pd
+
+st.header("My first Streamlit App")
+
+option = st.sidebar.selectbox(
+    'Choose an option',
+     ['Greet', 'Display a picture'])
+
+
+if option == 'Greet':
+    name = st.text_input('What is your name?')
+    if name == '':
+        st.write('')
+    else:
+        st.write(f'# Hello, {name}, have a great day!')
+        
+
+else:
+    try:
+        url = st.text_input('Insert the URL of the image')
+        st.image(url)
+    except:
+        st.error('Please paste a URL')
